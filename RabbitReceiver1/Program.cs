38da31f4ece5a7879e9cafa6e7c207ced1dc77f0
Cherbaki Zondikize <samudcherbak2018@gmail.com>
2024-05-20 10:46:00 +0400

    Add project files.

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Receiver App1";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+string exchangeName = "exchange1";
+string routingName = "routingKey1";
+string queueName = "queue1";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingName, null);
+
+channel.BasicQos(0, 1, false);
+
+EventingBasicConsumer consumer = new(channel);
+consumer.Received += async (sender, args) =>
+{
+    byte[] body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"(R1)Processing message...: {message}");
+    await Task.Delay(1000);
+    Console.WriteLine($"(R1)Message processed");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+var consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+channel.BasicCancel(consumerTag);
+channel.Close();
+connection.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Sender App";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "exchange1";
+string routingName = "routingKey1";
+string queueName = "queue1";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false,null);
+channel.QueueBind(queueName, exchangeName, routingName,null);
+
+for(int i = 1; i <= 30; i++)
+{
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingName, null, messageBodyBytes);
+    await Task.Delay(200);
+}
+
+channel.Close();
+connection.Close();
