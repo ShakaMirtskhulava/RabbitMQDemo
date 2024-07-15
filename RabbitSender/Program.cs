9e5e2b7ea74e3ef07829b0de8792b4fd75ba7b72
Cherbaki Zondikize <samudcherbak2018@gmail.com>
2024-07-15 19:16:36 +0400

    Add Topic Exchange Example

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,4 +1,39 @@
-﻿using RabbitMQ.Client;
+﻿
+//Direct Exchange Example
+/*
+using RabbitMQ.Client;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Sender App";
+connectionFactory.VirtualHost = "newVHost1";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string queueName = "queue1";
+string exchangeName = "exchange1";
+string routingName = "routingKey1";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false,null);
+channel.QueueBind(queueName, exchangeName, routingName,null);
+
+for(int i = 1; i <= 30; i++)
+{
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingName, null, messageBodyBytes);
+    await Task.Delay(200);
+}
+
+channel.Close();
+connection.Close();
+*/
+
+//Topic Exchange Example
+using RabbitMQ.Client;
 using System.Text;
 
 string userName = "guest", password = "guest", queueMessagesPort = "5672";
@@ -9,18 +44,15 @@ connectionFactory.ClientProvidedName = "RabbitMQ Sender App";
 IConnection connection = connectionFactory.CreateConnection();
 IModel channel = connection.CreateModel();
 
+
 string exchangeName = "exchange1";
-string routingName = "routingKey1";
-string queueName = "queue1";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
 
-channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
-channel.QueueDeclare(queueName, true, false, false,null);
-channel.QueueBind(queueName, exchangeName, routingName,null);
-
-for(int i = 1; i <= 30; i++)
+for (int i = 1; i <= 30; i++)
 {
+    string routingKey = i % 2 == 0 ? "key.even" : "key.odd"; // Routing key for message
     byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
-    channel.BasicPublish(exchangeName, routingName, null, messageBodyBytes);
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
     await Task.Delay(200);
 }
 
--- RabiMQtReceiver2/Program.cs
+++ RabiMQtReceiver2/Program.cs
@@ -1,4 +1,51 @@
-﻿using RabbitMQ.Client;
+﻿
+//Direct Exchange Example
+/*
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Receiver App2";
+connectionFactory.VirtualHost = "newVHost1";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+string exchangeName = "exchange1";
+string routingName = "routingKey1";
+string queueName = "queue1";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingName, null);
+
+channel.BasicQos(0, 1, false);
+
+EventingBasicConsumer consumer = new(channel);
+consumer.Received += async (sender, args) =>
+{
+    byte[] body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"(R2)Processing message...: {message}");
+    await Task.Delay(2000);
+    Console.WriteLine($"(R2)Message processed");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+var consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+channel.BasicCancel(consumerTag);
+channel.Close();
+connection.Close();
+*/
+
+//Topic Exchange Example
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
@@ -9,12 +56,15 @@ connectionFactory.ClientProvidedName = "RabbitMQ Receiver App2";
 
 IConnection connection = connectionFactory.CreateConnection();
 IModel channel = connection.CreateModel();
+
 string exchangeName = "exchange1";
-string routingName = "routingKey1";
-string queueName = "queue1";
-channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+string queueName = "queue2";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
 channel.QueueDeclare(queueName, true, false, false, null);
-channel.QueueBind(queueName, exchangeName, routingName, null);
+
+// Bind queue to exchange with a routing key pattern
+string routingPattern = "key.odd"; // Will receive messages with "key.odd" routing key
+channel.QueueBind(queueName, exchangeName, routingPattern);
 
 channel.BasicQos(0, 1, false);
 
