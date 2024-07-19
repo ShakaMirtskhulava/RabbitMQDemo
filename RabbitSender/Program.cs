54bf760427ac269207b4b417940bfd30068439e2
Shaka Mirtskhulava <102857931+ShakaMirtskhulava@users.noreply.github.com>
2024-07-19 13:16:52 +0400

    Update Program.cs

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 //Direct Exchange Example
 /*
 using RabbitMQ.Client;
@@ -32,7 +32,9 @@ channel.Close();
 connection.Close();
 */
 
-//Topic Exchange Example
+
+//Topic Exchange Example Sender
+/*
 using RabbitMQ.Client;
 using System.Text;
 
@@ -43,7 +45,6 @@ connectionFactory.ClientProvidedName = "RabbitMQ Sender App";
 
 IConnection connection = connectionFactory.CreateConnection();
 IModel channel = connection.CreateModel();
-
 
 string exchangeName = "exchange1";
 channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
@@ -58,3 +59,69 @@ for (int i = 1; i <= 30; i++)
 
 channel.Close();
 connection.Close();
+*/
+
+// Header Exchange Example Sender
+/*
+using RabbitMQ.Client;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Header Sender App";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "header_exchange1";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Headers);
+
+for (int i = 1; i <= 10; i++)
+{
+    var properties = channel.CreateBasicProperties();
+    properties.Headers = new Dictionary<string, object>();
+
+    if (i % 2 == 0)
+        properties.Headers.Add("format", "pdf");
+    else
+        properties.Headers.Add("format", "doc");
+
+    string message = $"Message #{i}";
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchangeName, string.Empty, properties, messageBodyBytes);
+    await Task.Delay(200);
+}
+
+channel.Close();
+connection.Close();
+*/
+
+
+// Fanout Exchange Example Sender
+/*
+using RabbitMQ.Client;
+using System.Text;
+
+string userName = "guest", password = "guest", queueMessagesPort = "5672";
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new Uri($"amqp://{userName}:{password}@localhost:{queueMessagesPort}");
+connectionFactory.ClientProvidedName = "RabbitMQ Fanout Sender App";
+
+IConnection connection = connectionFactory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "fanout_exchange1";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+for (int i = 1; i <= 10; i++)
+{
+    string message = $"Message #{i}";
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchangeName, string.Empty, null, messageBodyBytes);
+    await Task.Delay(200);
+}
+
+channel.Close();
+connection.Close();
+*/
